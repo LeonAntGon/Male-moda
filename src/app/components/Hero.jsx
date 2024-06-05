f6d82fbd2aa6a3119e9c2b58e509595eab2f7a4c
unknown <gonzalezleonardoantonio056@gmail.com>
2024-06-05 15:25:08 -0300

    bg images hero

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -2,7 +2,12 @@ import bg from '@/public/bg-hero-2.jpg';
 import { Saira_Stencil_One, Saira_Condensed } from 'next/font/google';
 import '../styles/hero.css'
 import Link from 'next/link'
-
+import remera from '@/public/sections/remera.jpg';
+import pants from '@/public/sections/jean.jpg';
+import campera from '@/public/sections/camperas.jpg';
+import sweater from '@/public/sections/sweater.jpg';
+import dress  from '@/public/sections/vestidos.jpg';
+import shorts from '@/public/sections/pollera.jpg';
 
 const saira = Saira_Condensed({
     weight: "700",
@@ -42,35 +47,41 @@ export default function Hero(){
                 <div className='container mt-4'>
                 
                     <Link href="/remeras">
-                        <div className="cont1">
+                        <div className="cont1"
+                        style={{ backgroundImage: `url(${remera.src})`}}>
                         <h4 className={saira.className}>REMERAS & TOP</h4>
                         </div>
                     </Link>
                     
                     <Link href="/pantalonesyjeans">
-                    <div className="cont2">
+                    <div className="cont2"
+                    style={{ backgroundImage: `url(${pants.src})`}}>
                         <h4 className={saira.className}>PANTALONES & JEANS</h4>
                     </div>
                     </Link>
 
                     <Link href="/camperas">
-                    <div className="cont3">
+                    <div className="cont3"
+                    style={{ backgroundImage: `url(${campera.src})`}}>
                         <h4 className={saira.className}>CAMPERAS</h4>
                     </div>
                     </Link>
 
                     <Link href="/sweaters">
-                    <div className="cont4">
+                    <div className="cont4"
+                    style={{ backgroundImage: `url(${sweater.src})`}}>
                         <h4 className={saira.className}>SWEATERS</h4>
                     </div>
                     </Link>
                     <Link href="/conjuntos">
-                    <div className="cont5">
+                    <div className="cont5"
+                    style={{ backgroundImage: `url(${dress.src})`}}>
                         <h4 className={saira.className}>VESTIDOS</h4>
                     </div>
                     </Link>
                     <Link href="/vestidos">
-                    <div className="cont6">
+                    <div className="cont6"
+                    style={{ backgroundImage: `url(${shorts.src})`}}>
                         <h4 className={saira.className}>POLLERAS & SHORTS</h4>
                     </div>
                     </Link>
