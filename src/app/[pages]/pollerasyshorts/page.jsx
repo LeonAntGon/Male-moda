a9cbe205f10e30f7d25052cb9fea22e9f7f0ede9
unknown <gonzalezleonardoantonio056@gmail.com>
2024-06-06 05:50:06 -0300

    lets fix

--- src/app/[pages]/pollerasyshorts/page.jsx
+++ src/app/[pages]/pollerasyshorts/page.jsx
@@ -1,29 +1,14 @@
 import Footer from "../../components/Footer"
 import Nav from "../../components/Nav"
-import Pagestitles from "@/src/app/components/Pagestitles";
 import { Image } from "@nextui-org/react";
+import Pagestitles from "@/src/app/components/Pagestitles";
 
 export default function Pollerasyshorts(){
     return(
         <>
         <Nav/>
         <Pagestitles text="POLLERAS Y SHORTS"/>
-        <section className="z-10 flex flex-wrap justify-center gap-2 gap-y-[50px] mx-[10px] h-auto mt-6">
-                {products.map((product) => (
-                    <div className='inline-block min-w-[100px] min-h-[120px] max-w-[160px] ' key={product.id}>
-                         <img
-                          className=" w-full h-full rounded-md"
-                          src={product.image.src}
-                          alt="prenda"/>
-
-                        <div className="[&>p]:text-center">
-                            <p className=' text-xs font-bold '>{ product.title }</p>
-                            <p className=' text-xs font-medium '>{ product.brand }</p>
-                            <p className=' text-xs font-light '>{ product.price }</p>
-                        </div>
-                    </div>
-                ))}
-            </section>
+        
         <Footer/>
         </>
     )
