6f3ad8308b6e4f46579d1055a17b5eb9285cf7b5
unknown <gonzalezleonardoantonio056@gmail.com>
2024-06-11 12:54:37 -0300

    social medias

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -21,12 +21,12 @@ export default function Footer(){
             <h3 className="text-center text-[16px] font-bold px-2">Siguenos</h3>
             
             <div className="flex mx-4 justify-center my-[25px]">
-            <a>
+            {/*<a>
             <div className="pr-4 cursor-pointer">
                 <svg  className="[&>path]:hover:stroke-blue-700" width="30px" height="30px" stroke-width="1.2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
                 <path  d="M17 2H14C12.6739 2 11.4021 2.52678 10.4645 3.46447C9.52678 4.40215 9 5.67392 9 7V10H6V14H9V22H13V14H16L17 10H13V7C13 6.73478 13.1054 6.48043 13.2929 6.29289C13.4804 6.10536 13.7348 6 14 6H17V2Z" stroke="#000000" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
             </div>
-            </a>
+            </a>*/}
 
            <a href="https://www.instagram.com/male_moda1"></a>
             <div className="pl-4 cursor-pointer">
