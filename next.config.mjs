77e223665ebb1c883504c5baf2d9637e7ef22565
unknown <gonzalezleonardoantonio056@gmail.com>
2024-06-14 22:03:38 -0300

    changes please

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      unoptimized: true, // Use this if needed
-    },
-    // Other configurations...
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  images: {
+      unoptimized: true,
+  },
+  output: 'export',
+};
+
+export default nextConfig;
   
