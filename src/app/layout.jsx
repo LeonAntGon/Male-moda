62b3712b2cae298f68757e597069269d3b73bc42
unknown <gonzalezleonardoantonio056@gmail.com>
2024-06-14 21:15:40 -0300

    changes bro

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={robo.className}>{children}</body>
+      <body className={robo.className}>
+        {children}
+      <SpeedInsights /> 
+      <Analytics /></body>
     </html>
   );
 }
