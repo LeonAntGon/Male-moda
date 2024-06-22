ac09c9153f6545c9961e2c987a5ddc5ef800e82c
unknown <gonzalezleonardoantonio056@gmail.com>
2024-06-22 11:13:42 -0300

    pantalones hdp

--- src/app/[pages]/pantalonesyjeans/data.js
+++ src/app/[pages]/pantalonesyjeans/data.js
@@ -1,5 +1,5 @@
 import uno from '@/public/pantalonesyjeans/8700.jpg';
-import dos from '@/public/pantalonesyjeans/13000.jpg';
+//import dos from '@/public/pantalonesyjeans/13000.jpg';
 
 import tres from '@/public/pantalonesyjeans/pantalonbengalinatalle36-18000.jpg';
 import cuatro from '@/public/pantalonesyjeans/pantalonbengalinatalle4018000.jpg';
@@ -15,38 +15,33 @@ export const productstwo = [
     },
     {
         id: 2,
-        image: dos,
-        price: "$13000",
+        image: tres,
+        title: "Pantalon de bengalina",
+        price: "$18000",
     },
     {
         id: 3,
-        image: tres,
-        title:"Pantalon de lanilla",
+        image: cuatro,
+        title: "Pantalon de bengalina",
         price: "$18000",
     },
     {
         id: 4,
-        image: cuatro,
-        title:"Pantalon de lanilla",
-        price: "$18000",
+        image: cinco,
+        title: "Pantalon de lanilla",
+        price: "$8700",
     },
     {
         id: 5,
-        image: cinco,
-        title:"Pantalon de lanilla",
+        image: seis,
+        title: "Pantalon de lanilla",
         price: "$8700",
     },
     {
         id: 6,
-        image: seis,
-        title:"Pantalon de lanilla",
-        price: "$8700",
-    },
-    {
-        id: 7,
         image: siete,
-        title:"Pantalon Wide Leg bengalina",
-        brand:"Talles 38-40-42-44",
+        title: "Pantalon Wide Leg bengalina",
+        brand: "Talles 38-40-42-44",
         price: "$22000",
-    },
+    }
 ]
